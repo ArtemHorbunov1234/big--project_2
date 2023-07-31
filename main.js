e3cda451a867936c8ed7a730726854ef2cdaf9d6
Artem <kolyasik124@ukr.net>
2023-07-31 22:07:56 +0300

    @media width: 768px header, section_1, section_2

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const toggleShow = document.getElementById('btn--show');
 const myDiv = document.getElementById('dropdown--show');
 const toggleHidden = document.getElementById('btn--hidden');
 const overlay = document.getElementById('overlay');
+const menuShow = document.getElementById('menu');
+const menuPanel = document.getElementById('dropdown__hamburger-menu');
 
 toggleShow.addEventListener('click', function () {
     myDiv.style.display = 'inherit';
@@ -11,4 +13,15 @@ toggleShow.addEventListener('click', function () {
 toggleHidden.addEventListener('click', function () {
     myDiv.style.display = 'none';
     overlay.style.display = 'none';
+});
+
+menuShow.addEventListener('click', function () {
+    if (menuShow.src.match('/dist/img/header--hamburger-menu.svg')) {
+        menuShow.src = '/dist/img/dropdown__hamburger-menu--cancel.svg';
+        menuPanel.style.display = 'inherit';
+    } else {
+        menuPanel.style.display = 'none';
+
+        menuShow.src = '/dist/img/header--hamburger-menu.svg';
+    }
 });
